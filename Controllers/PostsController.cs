e8b75ad0406d3617f0d593436691bd27f96b7f3f
Baptiste Geffrault <bgeffrault@outlook.fr>
2020-02-08 21:32:26 -0500

    Functionnal Posts API

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mini_Blog_Backend.Models;
+using MySql.Data.MySqlClient; //For connexion with a database
+
+namespace Mini_Blog_Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        // GET: api/Posts
+        [HttpGet]
+        public IEnumerable<Post> Get()
+        {
+            var list = new List<Post>();
+
+            MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=mini_blog_mydb;user=Baptiste;password=batdu-35");
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM posts_table";
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var post = new Post();
+                post.Id = reader.GetString(0);
+                post.Title = reader.GetString(1);
+                post.Content = reader.GetString(2);
+                post.Created_at = reader.GetString(3);
+                post.LoveIts = reader.GetString(4);
+                post.Photo = reader.GetString(5);
+                post.PhotoCropped = reader.GetString(6);
+
+                list.Add(post);
+            }
+            reader.Close();
+            
+            return list;
+        }
+
+        // GET: api/Posts/id
+        [HttpGet("{id}", Name = "Get")]
+        public Post Get(int id)
+        {
+            Post post = new Post();
+            MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=mini_blog_mydb;user=Baptiste;password=batdu-35");
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM posts_table WHERE id=" +id;
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                
+                post.Id = reader.GetString(0);
+                post.Title = reader.GetString(1);
+                post.Content = reader.GetString(2);
+                post.Created_at = reader.GetString(3);
+                post.LoveIts = reader.GetString(4);
+                post.Photo = reader.GetString(5);
+                post.PhotoCropped = reader.GetString(6);
+            }
+            reader.Close();
+            return post;
+        }
+
+        // POST: api/Posts
+        [HttpPost]
+        public String Post([FromBody] Post post)
+        {
+            
+            MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=mini_blog_mydb;user=Baptiste;password=batdu-35");
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "INSERT INTO posts_table (title, content, created_at, loveIts, photo, photoCropped) " +
+                "VALUES('"+ post.ConvertForSQL(post.Title)+"', '"+
+                post.ConvertForSQL(post.Content) +"', '"+ 
+                post.Created_at +"', '"+ 
+                post.LoveIts +"', '"+ 
+                post.Photo + "', '" + 
+                post.PhotoCropped + "')";
+
+            MySqlDataReader reader = command.ExecuteReader();
+            return "Success";
+
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;database=mini_blog_mydb;user=Baptiste;password=batdu-35");
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM posts_table WHERE id=" + id;
+            MySqlDataReader reader = command.ExecuteReader();
+
+            return "Success";
+        }
+    }
+}
---
+++ Models/Posts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mini_Blog_Backend.Models
+{
+    public class Post
+    {
+		/*title: string; 
+		content: string;  
+		loveIts: number;
+		created_at: string;
+		photo: string;
+		photoCropped: string;*/
+
+		public string Title { get; set; }
+
+		public string Content { get; set; }
+
+		public string LoveIts { get; set; }
+
+		public string Created_at { get; set; }
+
+		public string Photo { get; set; }
+
+		public string PhotoCropped { get; set; }
+
+		public string Id { get; set; }
+
+		public string ConvertForSQL(String str)
+		{
+			str = str.Replace("'", "''");
+			return str;
+		}
+
+		public string ConvertForReturn(String str)
+		{
+			str = str.Replace("''", "'");
+			return str;
+		}
+	}
+}
